127641768b3e0b511024119087307973c8b22cbd
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2022-04-09 15:05:08 +0530

    Add files via upload

---
+++ SubString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SubString1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String : ");
+            string a = Console.ReadLine();
+
+            int len = a.Length;
+            Console.WriteLine("All substrings for given string are : ");
+
+
+            for (int i = 0; i < len; i++)
+            {
+
+                for (int j = 0; j < len - i; j++)
+                {
+                    Console.Write(a.Substring(i, j + 1) + " ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
